ed72d0ff0fc7cbcd1455591a36b0ac85fba57f05
elcabong <mbarone000@gmail.com>
2013-08-15 20:24:34 -0700

    added powermenu button

--- js/remote.js
+++ js/remote.js
@@ -1,6 +1,5 @@
 /*
 Remote for XBMC 
-Copyright 2010 Furya-creations.net
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,15 +14,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 /*
   Class manipulation
   http://www.openjs.com/scripts/dom/class_manipulation.php
 */
-
-
-
-
 function hasClass(ele,cls) {
   return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
 }
